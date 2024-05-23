c53357c871a986ecade230769bd3310f1823f08a
Wullems Bart <wullemsb@gmail.com>
2024-05-23 16:20:35 +0200

    First example, doesn't invoke the plugins

---
+++ EmailPlugin.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailRewriter
+{
+    public class EmailPlugin
+    {
+        [KernelFunction]
+        [Description("Sends an email to a recipient.")]
+        public async Task SendEmailAsync(
+            Kernel kernel,
+            [Description("Semicolon delimitated list of emails of the recipients")] string recipientEmails,
+            string subject,
+            string body
+        )
+        {
+            // Add logic to send an email using the recipientEmails, subject, and body
+            // For now, we'll just print out a success message to the console
+            Console.WriteLine("Email sent!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,31 +8,6 @@ using Microsoft.SemanticKernel.Plugins.Core;
 using System;
 using System.Net;
 
-Console.Write("Your request: ");
-//string request = Console.ReadLine()!;
-//string history = """
-//          <message role="user">I hate sending emails, no one ever reads them.</message>
-//          <message role="assistant">I'm sorry to hear that. Messages may be a better way to communicate.</message>
-//          """;
-
-//string prompt = $"""
-//         <message role="system">Instructions: What is the intent of this request?
-//         If you don't know the intent, don't guess; instead respond with "Unknown".
-//         Choices: SendEmail, SendMessage, CompleteTask, CreateDocument, Unknown.
-//         Bonus: You'll get $20 if you get this right.</message>
-        
-//         <message role="user">Can you send a very quick approval to the marketing team?</message>
-//         <message role="system">Intent:</message>
-//         <message role="assistant">SendMessage</message>
-        
-//         <message role="user">Can you send the full update to the marketing team?</message>
-//         <message role="system">Intent:</message>
-//         <message role="assistant">SendEmail</message>
-        
-//         {history}
-//         <message role="user">{request}</message>
-//         <message role="system">Intent:</message>
-//         """;
 #pragma warning disable SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 
@@ -44,163 +19,56 @@ var builder = Kernel.CreateBuilder()
         apiKey: null,
         endpoint: new Uri("http://localhost:11434"));// With Ollama OpenAI API endpoint
 
-builder.Services.AddLogging(c => c.SetMinimumLevel(LogLevel.Debug));
+// Create the kernel
+builder.Services.AddLogging(c => c.SetMinimumLevel(LogLevel.Trace).AddDebug());
+builder.Plugins.AddFromType<AuthorEmailPlanner>();
+builder.Plugins.AddFromType<EmailPlugin>();
+Kernel kernel = builder.Build();
 
-#pragma warning disable SKEXP0050 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-builder.Plugins
-        .AddFromType<Microsoft.SemanticKernel.Plugins.Core.MathPlugin>()
-        .AddFromType<ConversationSummaryPlugin>()
-        .AddFromType<TimePlugin>("time");
-#pragma warning restore SKEXP0050 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+// Retrieve the chat completion service from the kernel
+IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
-var kernel = builder.Build();
+// Create the chat history
+ChatHistory chatMessages = new ChatHistory("""
+You are a friendly assistant who likes to follow the rules. You will complete required steps
+and request approval before taking any consequential actions. If the user doesn't provide
+enough information for you to complete a task, you will keep asking questions until you have
+enough information to complete the task.
+""");
 
-var chat = kernel.CreateFunctionFromPrompt(
-    new PromptTemplateConfig()
-    {
-        Name = "Chat",
-        Description = "Chat with the assistant.",
-        Template = @"{{ConversationSummaryPlugin.SummarizeConversation $history}}
-                    User: {{$request}}
-                    Assistant: ",
-        TemplateFormat = "semantic-kernel",
-        InputVariables =
-        [
-            new() { Name = "history", Description = "The history of the conversation.", IsRequired = false, Default = "" },
-            new() { Name = "request", Description = "The user's request.", IsRequired = true }
-        ],
-        ExecutionSettings =
-        {
-            {
-                "default",
-                new OpenAIPromptExecutionSettings()
-                {
-                    MaxTokens = 1000,
-                    Temperature = 0.9
-                }
-            }
-        }
-    }
-);
-// Enable auto function calling
-//OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
-//{
-//    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
-//};
-
-//KernelArguments arguments = new KernelArguments(openAIPromptExecutionSettings);
-
-
-//Console.WriteLine(await kernel.InvokePromptAsync(prompt, arguments));
-//Console.ReadLine();
-
-
-
-ChatHistory history = [];
-
-// Start the chat loop
+// Start the conversation
 while (true)
 {
     // Get user input
-    Console.Write("User > ");
-    var request = Console.ReadLine();
+    System.Console.Write("User > ");
+    chatMessages.AddUserMessage(Console.ReadLine()!);
 
-    // Get chat response
-    var chatResult = kernel.InvokeStreamingAsync<StreamingChatMessageContent>(
-        chat,
-        new()
-        {
-            { "request", request },
-            { "history", string.Join("\n", history.Select(x => x.Role + ": " + x.Content)) }
-        }
-    );
-
-    // Stream the response
-    string message = "";
-    bool rolePrinted = false;
-
-    await foreach (var chunk in chatResult)
+    // Get the chat completions
+    OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
     {
-        if (chunk.Role.HasValue && !rolePrinted)
+        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+    };
+    var result = chatCompletionService.GetStreamingChatMessageContentsAsync(
+        chatMessages,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Stream the results
+    string fullMessage = "";
+    bool roleWritten = false;
+    await foreach (var content in result)
+    {
+        if (content.Role.HasValue && !roleWritten)
         {
-            Console.Write(chunk.Role + " > ");
-            rolePrinted = true;
+            System.Console.Write("Assistant > ");
+            roleWritten = true;
         }
-
-        message += chunk;
-        Console.Write(chunk);
+        System.Console.Write(content.Content);
+        fullMessage += content.Content;
     }
-    Console.WriteLine();
+    System.Console.WriteLine();
 
-    // Append to history
-    history.AddUserMessage(request!);
-    history.AddAssistantMessage(message);
+    // Add the message from the agent to the chat history
+    chatMessages.AddAssistantMessage(fullMessage);
 }
-
-// Test the math plugin
-//double answer = await kernel.InvokeAsync<double>(
-//"MathPlugin", "Sqrt",
-//    new() {
-//        { "number1", 12 }
-//    }
-//);
-//Console.WriteLine($"The square root of 12 is {answer}.");
-
-//const string promptTemplate = @"
-//Today is: {{time.Date}}
-//Current time is: {{time.Time}}
-
-//Answer to the following questions using JSON syntax, including the data used.
-//Is it morning, afternoon, evening, or night (morning/afternoon/evening/night)?
-//Is it weekend time (weekend/not weekend)?
-//What is the square root of 12?";
-
-//var results = await kernel.InvokePromptAsync(promptTemplate);
-//Console.WriteLine(results);
-
-//// Get chat completion service
-//var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
-//var history = new ChatHistory();
-
-//// Start the conversation
-//Console.Write("User > ");
-//string? userInput;
-//while ((userInput = Console.ReadLine()) != null)
-//{
-//    history.AddUserMessage(userInput);
-
-//    // Enable auto function calling
-//    OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
-//    {
-//        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
-//    };
-
-//    // Get the response from the AI
-//    var result = chatCompletionService.GetStreamingChatMessageContentsAsync(
-//                        history,
-//                        executionSettings: openAIPromptExecutionSettings,
-//                        kernel: kernel);
-
-//    // Stream the results
-//    string fullMessage = "";
-//    var first = true;
-//    await foreach (var content in result)
-//    {
-//        if (content.Role.HasValue && first)
-//        {
-//            Console.Write("Assistant > ");
-//            first = false;
-//        }
-//        Console.Write(content.Content);
-//        fullMessage += content.Content;
-//    }
-//    Console.WriteLine();
-
-//    // Add the message from the agent to the chat history
-//    history.AddAssistantMessage(fullMessage);
-
-//    // Get user input again
-//    Console.Write("User > ");
-//}
-
 #pragma warning restore SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
